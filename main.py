3ba87a542d508ff1bd33ceaad294c9b177d2c923
azerdeva270 <26114083-azerdeva270@users.noreply.replit.com>
2023-11-14 14:12:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# x = str(5)
+# print(type(x))
+# n = float(input())
+# print(n)
+# print(type(n))
+# line1 = 'Лев Толстой '
+# line2 = 'Анна Каренина'
+# print(line1 + line2)
+# print(line1 + ' ' + line2)
+# print(line1, line2)
+#numbers = '0123456789'
+#letters = 'Я программист!'
+#print(numbers, letters)
+
+##projects = ["Сайт для отеля", "Игра-стрелялка", "Калькулятор", "Видеохостинг"]
+#print(projects[])
+a = 'hello'
+b = 5
+c = a * b
+print(c)
